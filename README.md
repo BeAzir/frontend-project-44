c4de96003cff3d465694a473217c916dd610b6fe
BeAzir <bgannyk@yandex.ru>
2022-09-05 18:06:00 +0300

    9 step

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 asciinema link https://asciinema.org/a/518744
 asciinema brain-calc: https://asciinema.org/a/j82IWqlqVzAyj3hID5D05iNV6
 asciinema brain-gcd: https://asciinema.org/a/kSIXLMSQbVquvU8ZltJ9GHtpC
-asciinema brain-progression:https: //asciinema.org/a/osap21FjDC4Kh33RzjRy746F0
+asciinema brain-progression: https: //asciinema.org/a/osap21FjDC4Kh33RzjRy746F0
+asciinema brain-prime:  https://asciinema.org/a/LVTQnCtUKp9V4Yq7Rh8ji6sWM
 [![Actions Status](https://github.com/BeAzir/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/BeAzir/frontend-project-44/actions)
---
+++ bin/brain-prime.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+
+
+const primeOrNot = (number) => {
+	    const coll = [];
+	    for(let i = 1; i <= number; i+=1){
+		            if(number % i === 0){
+				                coll.push(i);
+				            }
+		        }
+
+	    if(coll.length === 2){
+		            return 'Prime'
+		        } else {
+				        return 'Not prime';
+				    };
+
+};
+
+const getRandom = (min, max) => {
+	    min = Math.ceil(min);
+	    max = Math.floor(max);
+	    return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+const brainPrime = () => {
+	    console.log('Welcome to the Brain Games!');
+	    const name = readlineSync.question("May i have your name? ");
+	    console.log(`Hello, ${name}!`);
+	    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+	    for(let i = 1; i <= 3; i += 1){
+		            const randomNumber = getRandom(1, 25);
+		            const CorrectAnswer = primeOrNot(randomNumber);
+		            console.log(`Question: ${randomNumber}`);
+		            const answer = readlineSync.question('Your answer: ');
+
+		            if(CorrectAnswer === 'Prime' && answer !== 'yes'){
+				                return console.log(`'No' is wrong asnwer ;(. Correct answer was 'yes'.\n Lets try again ${name}`)
+				            } else if (CorrectAnswer === 'Not prime' && answer !== 'no'){
+						                return console.log(`'yes' is wrong asnwer ;(. Correct answer was 'no'.\n Lets try again ${name}`)
+						            }
+		            console.log('Correct!');
+
+		            if(i === 3){
+				                return console.log(`Congratulations, ${name}!`);
+				            }
+		        }
+};
+brainPrime();
