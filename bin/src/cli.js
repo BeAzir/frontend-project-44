a5fba642819441b55b2b823f50fd21fe20633eca
BeAzir <bgannyk@yandex.ru>
2022-09-07 21:12:42 +0300

    fixed cli.js

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 function greeting() {
-  const name = readlineSync.question("May I have your name? ");
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-	};
+  }
 export default greeting;
