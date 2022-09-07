cd2213a176b95bf353aef28ab1d4942e22dd8aa0
BeAzir <bgannyk@yandex.ru>
2022-09-07 17:51:40 +0300

    fixed linter

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -4,42 +4,40 @@ import readlineSync from 'readline-sync';
 
 const gcd = (x, y) => {
   if (y > x) {
-    return gcd(y, x);
+  return gcd(y, x);
   }
   if (!y) {
-    return x;
+  return x;
   }
-    return gcd(y, x % y);
+  return gcd(y, x % y);
 };
 
 function getRandom(min, max) {
   return Math.round((Math.random() * (max - min)));
-};
+}
 
 function brainGcd() {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May i have your name? ');
-    console.log(`Hello, ${name}!`);
-    console.log('Find the greatest common divisor of given numbers.');
-    
-    for (let i = 1; i <= 3; i +=1) {
-        const randomNum1 = getRandom(0, 100);
-        const randomNum2 = getRandom(0, 25);
-            console.log(`Question: ${randomNum1} ${randomNum2}`);
-            const answer = readlineSync.question('Your answer: ');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Find the greatest common divisor of given numbers.');
+  for (let i = 1; i <= 3; i += 1) {
+    const randomNum1 = getRandom(0, 100);
+    const randomNum2 = getRandom(0, 25);
+    console.log(`Question: ${randomNum1} ${randomNum2}`);
+    const answer = readlineSync.question('Your answer: ');
 
-        if (answer === String(gcd(randomNum1, randomNum2))) {
-        console.log('Correct!')
+    if (answer === String(gcd(randomNum1, randomNum2))) {
+      console.log('Correct!');
     } else {
-           const correctAnswer = gcd(randomNum1, randomNum2);
-            return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
-        }
-
-        if(i === 3){
-        return console.log(`Congratulations, ${name}!`)
+      const correctAnswer = gcd(randomNum1, randomNum2);
+      return console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${name}!`);
     }
 
-        }
+    if (i === 3) {
+      return console.log(`Congratulations, ${name}!`);
+    }
+  }
 }
 brainGcd();
 	
