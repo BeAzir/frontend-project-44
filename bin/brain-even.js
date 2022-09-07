e60b6431dc4635276ed6564e8ad680e9c17ceb19
BeAzir <bgannyk@yandex.ru>
2022-09-07 17:22:17 +0300

    fixed linter

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,23 +5,23 @@ const brainEven = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for(let i = 1; i <= 3; i += 1){
+  for (let i = 1; i <= 3; i += 1) {
     const randomNumber = Math.random() * 10;
     const roundNumber = Math.round(randomNumber);
 
     console.log(`Question: ${roundNumber}`);
     const answer = readlineSync.question('Your answer: ');
 
-        if (roundNumber % 2 === 0 && answer !== 'yes') {
-            return console.log(`'No' is wrong asnwer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
+    if (roundNumber % 2 === 0 && answer !== 'yes') {
+      return console.log(`'No' is wrong asnwer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
     } else if (roundNumber % 2 !== 0 && answer !== 'no') {
       return console.log(`'yes' is wrong asnwer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
     }
-      console.log('Correct!');
+    console.log('Correct!');
 
-        if (i === 3) {
-            return console.log(`Congratulations, ${name}!`);
-        }
-    };
+    if (i === 3) {
+      return console.log(`Congratulations, ${name}!`);
+  }
+  }
 };
 brainEven();
