22df84fb0f3136966e1ef07acefd356cc0ac5a42
Bored Bogdan <112168726+BeAzir@users.noreply.github.com>
2022-10-21 22:49:38 +0300

    Update brain-calc.js

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -5,8 +5,7 @@ import readlineSync from 'readline-sync';
 function getRandom(min, max) { return Math.round(Math.random() * (max - min)); }
 function getRandomSign() {
   const signs = ['-', '+', '*'];
-  const length = signs.length;
-  const generate = Math.floor(Math.random() * length);
+  const generate = Math.floor(Math.random() * signs.length);
   const randomValue = signs[generate];
   return randomValue;
 }
@@ -23,7 +22,7 @@ const mutant = (Num1, Sign, Num2) => {
     return Num1 * Num2;
   }
   return undefined
-};
+}
 function brainCalc() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May i have your name? ');
@@ -37,15 +36,16 @@ function brainCalc() {
     const answer = readlineSync.question('Your answer: ');
 
     if (answer === String(mutant(randomNum1, randomSign, randomNum2))) {
-      console.log('Correct!');
+      let output = 'Correct!';
     } else {
       const CorrectAnswer = String(mutant(randomNum1, randomSign, randomNum2));
       const wrongAnswer = String(answer);
-      return console.log(`${wrongAnswer} is wrong answer ;(. Correct answer was ${CorrectAnswer}.\nLet's try again, ${name}!`);
+      output = `${wrongAnswer} is wrong answer ;(. Correct answer was ${CorrectAnswer}.\nLet's try again, ${name}!`
     }
     if (i === 3) {
-      console.log(`Congratulations, ${name}!`);
+      output = `Congratulations, ${name}!`;
     }
+    return output;
   }
 }
 
