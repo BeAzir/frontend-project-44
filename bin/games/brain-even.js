e04e5f8b1557fe834ed3e99001d3f62f45d08fc2
Bored Bogdan <112168726+BeAzir@users.noreply.github.com>
2022-10-22 16:27:10 +0300

    Update brain-even.js

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -14,14 +14,13 @@ function brainEven() {
 
     if (roundNumber % 2 === 0 && answer !== 'yes') {
       return console.log(`'No' is wrong asnwer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
-    } else if (roundNumber % 2 !== 0 && answer !== 'no') {
+    }
+    
+    if (roundNumber % 2 !== 0 && answer !== 'no') {
       return console.log(`'yes' is wrong asnwer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
     }
     console.log('Correct!');
-
-    if (i === 3) {
-      return console.log(`Congratulations, ${name}!`);
-    }
   }
+  return console.log(`Congratulations, ${name}!`);
 }
 brainEven();
